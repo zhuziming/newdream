bc54b75ef0daef60f21bacdee8e98dcdaa23a341
mldn <739706661@qq.com>
2018-03-15 16:19:46 +0800

    change hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello GIT");
 		System.out.println("github");
-		System.out.println("commit");
+		System.out.println("master branch");
 	}
 }
